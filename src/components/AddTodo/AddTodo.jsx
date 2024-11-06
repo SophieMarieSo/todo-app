1018aa668aea9597f2051e96f49f5fc1a442e6b3
AhnEunJin <modic970515@gmail.com>
2024-11-06 12:31:25 +0900

    unique id 설정

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState('');
@@ -10,7 +11,7 @@ export default function AddTodo({ onAdd }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text.trim().length === 0) return;
-    onAdd({ id: '3', text, status: 'active' });
+    onAdd({ id: uuid(), text, status: 'active' });
     setText('');
   };
 
