68d8c5a49dd6c931b1d932165aaee435c94dcae1
AhnEunJin <modic970515@gmail.com>
2024-11-13 12:33:48 +0900

    toggle button(dark mode) styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ filters, filter, onFilterChange }) {
   const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <header className={styles.header}>
-      <button onClick={toggleDarkMode}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
         {!darkMode ? <HiMoon /> : <HiSun />}
       </button>
       <ul className={styles.filters}>
